643ff4136d0919f5e0e86d08dda76133cc03b900
Vitas Remeykis <ccloudyy74@gmail.com>
2026-09-24 10:38:58 +0300

    add test annotaions

--- src/main/java/company/vk/edu/distrib/compute/cloudyy74/urlshortener/Cloudyy74UrlShortenerServiceFactory.java
+++ src/main/java/company/vk/edu/distrib/compute/cloudyy74/urlshortener/Cloudyy74UrlShortenerServiceFactory.java
@@ -1,9 +1,13 @@
 package company.vk.edu.distrib.compute.cloudyy74.urlshortener;
 
 import company.vk.edu.distrib.compute.AbstractHttpServiceFactory;
+import company.vk.edu.distrib.compute.urlshortener.UrlShortenerAuthTest;
+import company.vk.edu.distrib.compute.urlshortener.UrlShortenerTest;
 
 import java.io.IOException;
 
+@UrlShortenerTest
+@UrlShortenerAuthTest
 public class Cloudyy74UrlShortenerServiceFactory extends AbstractHttpServiceFactory<Cloudyy74UrlShortenerService> {
     @Override
     protected Cloudyy74UrlShortenerService doCreate(int port) throws IOException {
